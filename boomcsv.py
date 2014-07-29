8db801bf5ede2326ce4e8adbf2dcac6af1b65852
derak <derak@opendns.com>
2014-07-29 10:53:24 -0700

    Command line args to add and check
    Added some command line functionality when running as a script.

--- boomcsv.py
+++ boomcsv.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python
 
+"""Basic module for interacting with CSV files.
+
+Copyright (c) 2014 Derak Berreyesa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+__author__ = "Derak Berreyesa (github.com/derak)"
+__version__ = "1.0"
+
+
 import sys
 import csv
 import operator
@@ -41,19 +68,30 @@ class BoomCSV(object):
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print "ERROR - You must provide a check parameter\n"
+    if len(sys.argv) < 2:
+        print "ERROR - You must provide some parameters\n"
         sys.exit(1)
 
     csv_file = 'some_file.csv'
-    check_field = sys.argv[1]
-
     bcsv = BoomCSV(csv_file)
-    print bcsv.check_csv(check_field)
 
-    # how to add a line
-    #bcsv.add_line('Joe Bob','joebob12@mycompany.com')
+    if sys.argv[1] == 'check':
+        if len(sys.argv) != 3:
+            print "ERROR - You must provide a check parameter\n"
+            print "python boomcsv.py check \"joe@bob.com\""
+            sys.exit(1)
+        
+        check_field = sys.argv[2]
+        print bcsv.check_csv(check_field)
 
-    # how to sort
-    #bcsv.sort(csv_file, 'some_file.csv')
+    if sys.argv[1] == 'add':
+        if len(sys.argv) != 4:
+            print "ERROR - To add you must also provide name and email"
+            print "python boomcsv.py add \"Joe Bob\" \"joe@bob.com\""
+            sys.exit(1)
+
+        name = sys.argv[2]
+        email = sys.argv[3]
+        bcsv.add_line(name,email)
+        bcsv.sort(csv_file, csv_file)
 
